2e409895bb4a4675242ff3f3eea8d8fbde036204
pedro-hos <pedro-hos@outlook.com>
2016-01-29 22:23:33 -0200

    erro corrigido obrigado Paulo Luan

--- retro-board-web/app/models/Card.js
+++ retro-board-web/app/models/Card.js
@@ -1,22 +1,30 @@
 var mongoose = require('mongoose');
+var findOrCreate = require('mongoose-findorcreate')
 
 module.exports = function() {
+
   var schema = mongoose.Schema({
-    
+
     type: { 
       type: String, 
-      required: true
-    },
-
-    comments: {
-      type: String, 
-      required: true, 
+      required: true,
       index: {
         unique: false
       }
-    }
+    }, 
 
+    comment: { 
+      type: String,
+      required: true,
+    },
+
+    insertion: {
+      type: Date, 
+      default: Date.now
+    }
   });
 
+  schema.plugin(findOrCreate);
   return mongoose.model('Card', schema);
+
 };
